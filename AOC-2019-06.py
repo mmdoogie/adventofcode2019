5292de9cae1f114d600c47fcc84c05fc2dc7f884
Matthew McDougal <24367033+mmdoogie@users.noreply.github.com>
2023-11-08 23:22:40 -0600

    Extract Djikstra function

--- AOC-2019-06.py
+++ AOC-2019-06.py
@@ -1,3 +1,5 @@
+from Djikstra import Djikstra
+
 with open('data/aoc-2019-06.txt') as f:
     dat = [x.strip() for x in f.readlines()]
 
@@ -20,41 +22,8 @@ for a, b in pairs:
     else:
         deps[b] = [a]
 
-def djikstra(point_set, neighbors_dict, start_point, end_point = None):
-    visited = set()
-    weights = dict()
-    paths = dict()
-
-    curr_point = start_point
-    weights[curr_point] = 0
-    paths[curr_point] = [curr_point]
-    
-    explore = dict()
-    explore[curr_point] = 0
-
-    while len(visited) < len(point_set):
-        del(explore[curr_point])
-        curr_weight = weights[curr_point] + 1
-        currPath = paths[curr_point] 
-        visited.add(curr_point)
-        if end_point is not None and curr_point == end_point:
-            break
-        if curr_point in neighbors_dict:
-            for n in neighbors_dict[curr_point]:
-                if n not in weights or curr_weight < weights[n]:
-                    weights[n] = curr_weight
-                    paths[n] = currPath + [n]
-                if n not in visited:
-                    explore[n] = curr_weight
-        cand = sorted(list(explore.items()), key=lambda x: x[1])
-        if len(cand) == 0:
-            break
-        curr_point, curr_weight = cand[0]
-
-    return weights, paths
-
-com_weights, com_paths = djikstra(objs, deps, 'COM')
+com_weights, com_paths = Djikstra(objs, deps, 'COM')
 print('Part 1:', sum(com_weights.values()))
 
-you_weights, you_paths = djikstra(objs, deps, 'YOU')
+you_weights, you_paths = Djikstra(objs, deps, 'YOU')
 print('Part 2:', you_weights['SAN'] - 2)
---
+++ Djikstra.py
@@ -0,0 +1,32 @@
+def Djikstra(point_set, neighbors_dict, start_point, end_point = None):
+    visited = set()
+    weights = dict()
+    paths = dict()
+
+    curr_point = start_point
+    weights[curr_point] = 0
+    paths[curr_point] = [curr_point]
+    
+    explore = dict()
+    explore[curr_point] = 0
+
+    while len(visited) < len(point_set):
+        del(explore[curr_point])
+        curr_weight = weights[curr_point] + 1
+        currPath = paths[curr_point] 
+        visited.add(curr_point)
+        if end_point is not None and curr_point == end_point:
+            break
+        if curr_point in neighbors_dict:
+            for n in neighbors_dict[curr_point]:
+                if n not in weights or curr_weight < weights[n]:
+                    weights[n] = curr_weight
+                    paths[n] = currPath + [n]
+                if n not in visited:
+                    explore[n] = curr_weight
+        cand = sorted(list(explore.items()), key=lambda x: x[1])
+        if len(cand) == 0:
+            break
+        curr_point, curr_weight = cand[0]
+
+    return weights, paths
